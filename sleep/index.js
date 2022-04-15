0fb07aa485ee2f11c4392a2acd63a4db1811f79b
xiyue <xiyuesaves@gmail.com>
2022-04-15 17:56:50 +0800

    更新睡觉插件

--- sleep/index.js
+++ sleep/index.js
@@ -1,6 +1,68 @@
 const { join } = require("path");
 const db = require("better-sqlite3")(join(process.cwd(), "database/sleep.db"));
-let onWeak = [];
+let onWeak = [],
+  sleepText = [
+    { text: "愿好梦", ran: 0.5 },
+    { text: "睡个好觉吧~", ran: 0.5 },
+    { text: "晚安啦~", ran: 0.5 },
+    { text: "今夜の月は綺麗ですね", ran: 0.01 }
+  ],
+  weekUpText = [
+    { text: "早上好", ran: 0.02 },
+    { text: "早安呀", ran: 0.01 }
+  ],
+  noSleepText = [
+    { text: "猪猪都没你能睡！", ran: 0.1 },
+    { text: "别睡了别睡了", ran: 0.2 },
+    { text: "再睡成猪了", ran: 0.2 },
+    { text: "你才睡醒没多久呢，等会再睡吧", ran: 0.8 }
+  ],
+  drpText = [
+    { text: "你因为睡得太久被踹下床了", ran: 0.5 },
+    { text: "你在睡觉时被丢进锅里炖掉了", ran: 0.4 },
+    { text: "你被做成应急食品了", ran: 0.2 },
+    { text: "你在睡觉时被UFO吸走了", ran: 0.4 },
+    { text: "你在睡觉时被做成了春卷！", ran: 0.3 },
+    { text: "你睡着睡着消失了！", ran: 0.1 },
+  ];
+
+function ranText(arr) {
+  let totalNum = 0,
+    numMap = [],
+    lastNum = 0,
+    fixNum = getFixNum(arr);
+  arr.forEach(el => {
+    totalNum += el.ran * fixNum;
+    numMap.push({ text: el.text, ran: [lastNum, totalNum] });
+    lastNum = totalNum + 1;
+  })
+  let ranNum = Math.floor(Math.random() * totalNum),
+    text = "";
+  numMap.forEach(el => {
+    if (ranNum >= el.ran[0] && el.ran[1] >= ranNum) {
+      text = el.text;
+    }
+  })
+  return text;
+}
+
+// 获取小数位数
+function getFixNum(arr) {
+  let num = 1,
+    ret = "1";
+  arr.forEach(el => {
+    if (el.ran.toString().split(".").length === 2) {
+      let thisNum = el.ran.toString().split(".")[1].length
+      if (thisNum > num) {
+        num = thisNum;
+      }
+    }
+  });
+  for (var i = 0; i < num; i++) {
+    ret += "0";
+  }
+  return ret * 1;
+}
 
 function getSleep(groupId, userId) {
 	return db.prepare("SELECT * FROM sleep WHERE groupId = ? AND userId = ?").get(groupId, userId);
@@ -69,9 +131,9 @@ function wakeUp(msg) {
 		sleepTime = thisTime - getSleep(groupId, userId).startTime;
 	onWeak.push({ "id": msg.sender.id, "weakTime": thisTime });
 	if (thisTime === sleepTime) {
-		msg.reply([{ type: "At", target: msg.sender.id }, { type: "Plain", text: `你因为睡得太久被踹下床了` }], msg);
+		msg.reply([{ type: "At", target: msg.sender.id }, { type: "Plain", text: ranText(drpText) }], msg);
 	} else {
-		msg.reply([{ type: "At", target: msg.sender.id }, { type: "Plain", text: `你醒了,睡了${getTime(sleepTime)}` }], msg);
+		msg.reply([{ type: "At", target: msg.sender.id }, { type: "Plain", text: `${ranText(weekUpText)} 你睡了${getTime(sleepTime)}` }], msg);
 		let totalSleep = db.prepare("SELECT * FROM sleep_ranking WHERE groupId = ? AND userId = ?").get(groupId, userId);
 		if (totalSleep) {
 			let totalTime = totalSleep.sleepTime + sleepTime;
@@ -86,12 +148,8 @@ function wakeUp(msg) {
 }
 
 function cleanBed(msg) {
-<<<<<<< HEAD
 	let maxSleepTime = 24 * 60 * 60 * 1000, // 一次性最多睡24小时
 		sleepWaiting = 2 * 60 * 1000, // 再次睡下前需要等待一定时间
-=======
-	let maxSleepTime = 24 * 60 * 60 * 1000, // 一次性最多睡12小时
->>>>>>> origin/main
 		groupId = msg.sender.group.id,
 		thisTime = new Date().getTime(),
 		groupBed = db.prepare("SELECT * FROM sleep WHERE groupId = ?").all(groupId);
@@ -148,9 +206,9 @@ const sleep = {
 						userName = msg.sender.memberName,
 						startTime = new Date().getTime();
 					let bed = db.prepare("INSERT INTO sleep VALUES (?,?,?,?)").run(groupId, userId, userName, startTime);
-					msg.reply([{ type: "Plain", text: "你睡下了" }], msg);
+					msg.reply([{ type: "Plain", text: ranText(sleepText) }], msg);
 				} else {
-					msg.reply([{ type: "Plain", text: "你才刚睡醒，等会再睡吧" }], msg);
+					msg.reply([{ type: "Plain", text: ranText(noSleepText) }], msg);
 				}
 			}
 		},
